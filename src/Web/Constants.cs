ba23e10dfd7f9ec8545ae8a3fe85cec90d7195e7
nenm <nam.nguyenduc@elca.vn>
2024-03-10 11:20:47 +0700

    init

---
+++ src/Web/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.eShopWeb.Web;
+ // Testing my PR
+public static class Constants
+{
+    public const string BASKET_COOKIENAME = "eShop";
+    public const int ITEMS_PER_PAGE = 10;
+    public const string DEFAULT_USERNAME = "Guest";
+    public const string BASKET_ID = "BasketId";
+}
